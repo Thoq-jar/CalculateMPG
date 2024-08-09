374757d5d63cf34910e01b99f4956a2a38fd8b17
Thoq-jar <tristanshaw000@gmail.com>
2024-08-09 11:14:10 -0400

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  imports: [
+    ReactiveFormsModule
+  ],
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'CalculateMPG';
+  mpg: string | null = null;
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      gallons: ['', Validators.required],
+      miles: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+    this.form.valueChanges.subscribe(values => {
+      const gallons = parseFloat(values.gallons);
+      const miles = parseFloat(values.miles);
+      if (!isNaN(gallons) && !isNaN(miles)) {
+        this.mpg = (miles / gallons).toFixed(2);
+      } else {
+        this.mpg = null;
+      }
+    });
+  }
 }
